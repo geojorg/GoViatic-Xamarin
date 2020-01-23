e7373e75c14d5b0cb76cb46e9a708757cd14cb56
Jorge Guerrero Montes <geojorg@gmail.com>
2020-01-23 13:44:58 -0500

    Update Visual

--- GoViatic/AssemblyInfo.cs
+++ GoViatic/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("ubuntu.ttf",Alias ="Ubuntu")]
-[assembly: ExportFont("ubuntubold.ttf", Alias = "UbuntuBold")]
+[assembly: ExportFont("Assets/ubuntu.ttf", Alias ="Ubuntu")]
+[assembly: ExportFont("Assets/ubuntubold.ttf", Alias ="UbuntuBold")]
